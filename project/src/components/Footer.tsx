5c702cfea9d91ecee5764f793b3af1e185bab667
ManasPatil281 <manaspatil281@gmail.com>
2025-03-04 18:07:34 +0530

    footer

--- project/src/components/Footer.tsx
+++ project/src/components/Footer.tsx
@@ -7,15 +7,9 @@ function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="flex items-center justify-center mb-8">
           <Briefcase className="w-8 h-8 text-purple-400" />
-<<<<<<< HEAD
-          <span className="ml-2 text-xl font-bold text-white">JobFlow AI for Talent Corner H.R. Services Pvt. Ltd.</span>
-        </div>
-        <p className="text-center text-gray-400">Made by Manas Patil .</p>
-=======
           <span className="ml-2 text-xl font-bold text-white">JobFlow AI for Talent Corner H.R. Services Pvt. Ltd. </span>
         </div>
         <p className="text-center text-gray-400"> Made by Manas Patil .</p>
->>>>>>> 4fb21972365b9bf13019cae804c687d1738e30ed
       </div>
     </footer>
   );
